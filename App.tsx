62971c91eef3bfb0cc102b31a6e66799993f76da
Nitish-Kes <nitish.kesarwani01@nagarro.com>
2025-01-31 11:32:07 +0530

    Status bar dynamic styling

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import {SafeAreaView,StatusBar,Text,useColorScheme} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from './src/utils/Colors';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: isDarkMode ? Colors.black : Colors.cyan,
   };
 
   return (
