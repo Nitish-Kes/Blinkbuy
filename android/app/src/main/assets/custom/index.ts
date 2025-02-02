560340eab2cf7a7d4f6ff176043f77f8df9430ca
Nitish-Kes <nitish.kesarwani01@nagarro.com>
2025-02-03 03:08:54 +0530

    Added custom fonts

---
+++ android/app/src/main/assets/custom/index.ts
@@ -0,0 +1,8 @@
+export abstract class Fonts {
+    static readonly Semibold: string = 'Poppins-SemiBold';
+    static readonly Regular: string = 'Poppins-Regular';
+    static readonly ExtraBold: string = 'Poppins-ExtraBold';
+    static readonly Bold: string = 'Poppins-Bold';
+    static readonly Medium: string = 'Poppins-Medium';
+    static readonly Light: string = 'Poppins-Light';
+  }
