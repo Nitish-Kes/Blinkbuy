70bca7a589112aaa20215d6829d95266c3f10372
Nitish-Kes <nitish.kesarwani01@nagarro.com>
2025-02-03 03:11:33 +0530

    Added react native elements styles

--- App.tsx
+++ App.tsx
@@ -1,29 +1,45 @@
 import React, { useEffect } from 'react';
-import {SafeAreaView,StatusBar,Text,useColorScheme} from 'react-native';
+import {SafeAreaView,StatusBar,useColorScheme} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 
 import { Colors } from './src/utils/Colors';
 import Register from './src/screens/Register/Register';
+import { makeStyles } from '@rneui/themed';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.black : Colors.blue,
-  };
+  const {
+    container,
+    statusBarStyle
+  } = useStyles(isDarkMode)
 
   useEffect(()=>{
     SplashScreen.hide()
   },[])
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={container}>
       <StatusBar
-        backgroundColor={backgroundStyle.backgroundColor}
+        backgroundColor={statusBarStyle.backgroundColor}
       />
-      <Register/>
+    <Register/>
     </SafeAreaView>
   );
 }
+
+type Props = {
+  isDarkMode?: boolean;
+};
+
+const useStyles = makeStyles((theme,props: Props)=>({
+  container: {
+    flex: 1,
+    backgroundColor: props.isDarkMode ? Colors.black : Colors.white
+  },
+  statusBarStyle: {
+    backgroundColor: props.isDarkMode ? Colors.black : Colors.blue,
+  }
+}))
 
 export default App;
